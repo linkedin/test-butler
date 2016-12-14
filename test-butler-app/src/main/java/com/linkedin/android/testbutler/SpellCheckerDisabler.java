848555b0c49318d896273a9ab2dd066caae4590c
Kyle Koser <kkoser@linkedin.com>
2016-12-13 16:30:42 -0800

    Add ability to enable and disable the system Spell Checker
    This adds the ability to toggle the spell checker setting for tests. This is useful as sometimes the spell checker dialog can appear during tests and cause tests to fail.
    
    No tests were added for this feature in this commit. Testing the spell checker is difficult to automate -- Android allows you to provide a replacement for the system spell checker, but it must be manually enabled by the user. Adding code to allow this in tests would add more code that could break, making the test more dangerous than productive.

---
+++ test-butler-app/src/main/java/com/linkedin/android/testbutler/SpellCheckerDisabler.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2016 LinkedIn Corp.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.linkedin.android.testbutler;
+
+import android.content.ContentResolver;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+/**
+ * A helper class for disabling or enabling the system spell checker
+ */
+public class SpellCheckerDisabler {
+
+    private static final String TAG = SpellCheckerDisabler.class.getSimpleName();
+
+    // The constant in Settings.Secure is marked with @hide, so we can't use it
+    private static final String SPELL_CHECKER_SETTING = "spell_checker_enabled";
+
+    private boolean originalSpellCheckerMode;
+
+    /**
+     * Should be called before starting tests, to save original spell checker state
+     */
+    @SuppressWarnings("deprecation")
+    void saveLocationServicesState(@NonNull ContentResolver contentResolver) {
+        try {
+            originalSpellCheckerMode = Settings.Secure.getInt(contentResolver, SPELL_CHECKER_SETTING) == 1;
+        } catch (Settings.SettingNotFoundException e) {
+            Log.e(TAG, "Error reading location mode settings!", e);
+        }
+    }
+
+    /**
+     * Should be called after testing completes, to restore original spell checker state
+     */
+    void restoreLocationServicesState(@NonNull ContentResolver contentResolver) {
+        setSpellChecker(contentResolver, originalSpellCheckerMode);
+    }
+
+    /**
+     * Enable or disable the system spell checker
+     * @param resolver the {@link ContentResolver} used to modify settings
+     * @param enabled The desired state of the Spell Checker service
+     * @return true if the value was set, false otherwise
+     */
+    public boolean setSpellChecker(@NonNull ContentResolver resolver, boolean enabled) {
+        int val = enabled ? 1 : 0;
+        return Settings.Secure.putInt(resolver, SPELL_CHECKER_SETTING, val);
+    }
+}
