89637c478c06b234df93feac342e9446717b8d7d
Walter Scott Johnson <wjohnson@linkedin.com>
2020-01-27 12:44:03 -0500

    Fix Physical Device Test Butler for API 21 and 22 (#101)
    * Add support for physical devices at API 21 and 22. Note: API <21 is not supported.
    Change device lookup by Build.SERIAL to fallback to logcat approach, because some serials do not match what ADB displays.
    Handle errors in ADB commands.
    
    * Address review comments

--- test-butler-app-physical-devices/src/main/java/com/linkedin/android/testbutler/shell/ActivityManagerWrapper.java
+++ test-butler-app-physical-devices/src/main/java/com/linkedin/android/testbutler/shell/ActivityManagerWrapper.java
@@ -15,9 +15,13 @@
  */
 package com.linkedin.android.testbutler.shell;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
+
+import androidx.annotation.RequiresApi;
 
 import com.linkedin.android.testbutler.utils.ExceptionCreator;
 
@@ -29,6 +33,7 @@ import java.lang.reflect.Method;
 class ActivityManagerWrapper {
 
     private static final String TAG = ActivityManagerWrapper.class.getSimpleName();
+    private static final int CURRENT_USER_ID = -2;
 
     private final Method broadcastIntent;
     private final Object iActivityManager;
@@ -38,15 +43,30 @@ class ActivityManagerWrapper {
         this.iActivityManager = iActivityManager;
     }
 
-    int broadcastIntent(Object iApplicationThreadCaller, Intent intent,
-                        String resolvedType, Object iIntentReceiverResultTo, int resultCode,
-                        String resultData, Bundle map, String[] requiredPermissions,
-                        int appOp, Bundle options, boolean serialized, boolean sticky,
-                        int userId) throws RemoteException {
+    void broadcastIntent(Intent intent) throws RemoteException {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            broadcastIntentPreM(intent);
+        } else {
+            broadcastIntentPostM(intent);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void broadcastIntentPostM(Intent intent) throws RemoteException {
         try {
-            return (int) broadcastIntent.invoke(iActivityManager, iApplicationThreadCaller, intent,
-                    resolvedType, iIntentReceiverResultTo, resultCode, resultData, map,
-                    requiredPermissions, appOp, options, serialized, sticky, userId);
+            broadcastIntent.invoke(iActivityManager, null, intent, intent.getType(), null, 0, null,
+                    null, null, -1, null, true, false, CURRENT_USER_ID);
+        } catch (Exception e) {
+            throw ExceptionCreator.createRemoteException(TAG, "Failed to broadcast intent", e);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
+    private void broadcastIntentPreM(Intent intent)
+            throws RemoteException {
+        try {
+            broadcastIntent.invoke(iActivityManager, null, intent, intent.getType(), null, 0, null,
+                    null, null, -1, true, false, CURRENT_USER_ID);
         } catch (Exception e) {
             throw ExceptionCreator.createRemoteException(TAG, "Failed to broadcast intent", e);
         }
@@ -60,11 +80,20 @@ class ActivityManagerWrapper {
 
             Object iActivityManager = activityManagerClass.getMethod("getDefault").invoke(null);
 
-            Method broadcastIntent = iActivityManager.getClass().getMethod("broadcastIntent",
-                    iApplicationThreadClass, Intent.class, String.class, iIntentReceiverClass,
-                    int.class, String.class, Bundle.class, String[].class, int.class, Bundle.class,
-                    boolean.class, boolean.class, int.class);
+            Method broadcastIntent;
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                broadcastIntent = iActivityManager.getClass().getMethod("broadcastIntent",
+                        iApplicationThreadClass, Intent.class, String.class, iIntentReceiverClass,
+                        int.class, String.class, Bundle.class, String.class, int.class,
+                        boolean.class, boolean.class, int.class);
+            } else {
+                broadcastIntent = iActivityManager.getClass().getMethod("broadcastIntent",
+                        iApplicationThreadClass, Intent.class, String.class, iIntentReceiverClass,
+                        int.class, String.class, Bundle.class, String[].class, int.class, Bundle.class,
+                        boolean.class, boolean.class, int.class);
+            }
             return new ActivityManagerWrapper(broadcastIntent, iActivityManager);
+
         } catch (Exception e) {
             throw new RuntimeException("Failed to initialize ActivityManagerWrapper", e);
         }
